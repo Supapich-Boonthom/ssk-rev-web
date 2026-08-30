7bfef1591e70167cbdbebb0c89d5e047f64306f4
Supapich-Boonthom <pattarawadee001425@gmail.com>
2026-08-30 22:00:47 +0700

    fix(vercel)

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 echo "Building project..."
-python3 -m pip install -r requirements.txt
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput --clear
+
 echo "Build finished!"
